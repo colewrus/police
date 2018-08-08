c39abfdbf30e23fd238f7caf74750f1e977372bd
colewrus <wrampelc@gmail.com>
2018-08-07 21:34:56 -0400

    wreck mapped

---
+++ Assets/Dilema.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+
+[CommandInfo("Dilema",
+             "First Test",
+             "Testing")]
+
+public class Dilema : Command {
+
+	public override void OnEnter(){
+		Debug.Log ("Test");
+		Continue();
+	}
+}
